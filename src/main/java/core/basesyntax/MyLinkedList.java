44e7068afde72158ef3809723978b3675d8b9c00
artem <artemnurmetow@gmail.com>
2025-09-15 22:30:42 +0200

    - added Node class to create Node objects to manipulate them in linkedList Class
    - added add method to add node objects together
    
    - toString to sysout properly

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,21 +3,61 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private Node<T> current;
+    private int size;
+
+
+    class Node<T> {
+        Node<T> next;
+        Node<T> prev;
+        T value;
+
+
+        public Node(Node<T> next, Node<T> prev, T value) {
+            this.next = next;
+            this.prev = prev;
+            this.value = value;
+        }
+
+    }
+
     @Override
     public void add(T value) {
+        if (size == 0) {
+            head = tail = new Node<>(null, null, value);
+        } else {
+            current = new Node<>(null, tail, value);
+            tail.next = current;
+            tail = current;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+
+        // Doesnt work
+        if (index == 0) {
+            head = tail = new Node<>(null, null, value);
+        } else {
+            current = new Node<>(tail.next, tail, value);
+            current.prev.next = current;
+            tail.prev = current;
+        }
+        size++;
+
     }
 
     @Override
     public void addAll(List<T> list) {
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public T get(int index) {
-        return null;
+        return (T) current;
     }
 
     @Override
@@ -35,13 +75,31 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return false;
     }
 
+
+    public String toString() {
+        if (head == null) {
+            return "[]";
+        }
+
+        StringBuilder result = new StringBuilder("[");
+        while (head != null) {
+            result.append(head.value);
+            if (head.next != null) {
+                result.append(", ");
+            }
+            head = head.next;
+        }
+        result.append("]");
+        return result.toString();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
